d64d6474bca4d9eb6f95600e798ce40e3a9d43a5
Andrea <75411510@continental.edu.pe>
2025-11-15 17:26:19 -0500

    Subiendo actualización de politicas de privacida, condiciones de servicios, Sobre nosotros, y ayuda

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,13 +3,21 @@ import { Routes, Route } from "react-router-dom";
 import SplashScreen from "../pages/SplashScreen";
 import Home from "../pages/Home";
 import EmotionFeed from "../pages/EmotionFeed";
+import Privacidad from "../pages/Privacidad";
+import CondicionesServicio from "../pages/CondicionesServicio";
+import SobreNosotros from "../pages/SobreNosotros";
+import Ayuda from "../pages/Ayuda";
 
 function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<SplashScreen />} />
             <Route path="/home" element={<Home />} />
-            <Route path="/emotion" element={<EmotionFeed />} />
+            <Route path="/emotion" element={<EmotionFeed />} />3
+            <Route path="/privacidad" element={<Privacidad />} />
+            <Route path="/condiciones" element={<CondicionesServicio />} />
+            <Route path="/sobre-nosotros" element={<SobreNosotros />} />
+            <Route path="/ayuda" element={<Ayuda />} />
         </Routes>
     );
     }
